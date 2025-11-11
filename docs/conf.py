f5d86deb64ec0d0de4051dbed69843d648474504
Andrew Back <andrew@abopen.com>
2025-11-11 16:49:26 +0000

    Add Pages build script+config

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# MyriadRF conf.py v1.0.0
 
 import sys
 import sphinx_rtd_theme
