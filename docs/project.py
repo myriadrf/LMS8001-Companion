356f7fe81557db3e70e6ea0586aaa5238692d954
Andrew Back <andrew@abopen.com>
2025-11-25 21:06:31 +0000

    Docs: update Sphinx config.

--- docs/project.py
+++ docs/project.py
@@ -9,22 +9,21 @@ release = '3v2'
 # github_repo_path should be set to the branch + path to the docs.
 
 github_repo = 'LMS8001-Companion'
-github_repo_path= 'master/docs/'
+github_repo_path = 'master/docs/'
 
 # The default language for syntax highlighting in code blocks.
 # This can be overridden using the ".. code-block::" directive.
 highlight_language = 'console'
 
 # Intersphinx mapping
-# For MyriadRF internal projects list the project slug only.
-# For external projects specify the reference and the full URL.
-# To minimise build time only include projects that are referenced!
+# To minimise build time only include projects that are referenced.
 intersphinx_internal = [
-    'limesuiteng',
+#    'suiteng',
 ]
-intersphinx_external = {
-    # 'numpy': 'https://numpy.org/doc/stable/',
-}
+
+intersphinx_external = [
+#    'sphinx',
+]
 
 # Set to True if the project is archived.
 archived = False
